afefa0928a4c6e510fe6773e54282d92f97d2c5a
vasujaiswal <98263094+vasujaiswal@users.noreply.github.com>
2022-04-27 00:56:01 +0530

    Add files via upload

---
+++ Code/MerkleTrees.java
@@ -0,0 +1,76 @@
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MerkleTrees {
+    List<String> txList;
+    String root;
+
+    
+    public MerkleTrees(List<String> txList) {
+        this.txList = txList;
+        root = "";
+    }
+
+    public void merkle_tree() {
+
+        List<String> tempTxList = new ArrayList<String>();
+
+        for (int i = 0; i <this.txList.size(); i++) {
+            tempTxList.add(this.txList.get(i));
+        }
+
+        List<String> newTxList = getNewTxList(tempTxList);
+
+        while (newTxList.size() != 1) {
+            newTxList = getNewTxList(newTxList);
+        }
+
+        this.root = newTxList.get(0);
+    }
+     
+    private List<String> getNewTxList(List<String> tempTxList) {
+
+        List<String> newTxList = new ArrayList<String>();
+        int index = 0;
+        while (index <tempTxList.size()) {
+           
+            String left = tempTxList.get(index);
+            index++;
+           
+            String right = "";
+            if (index != tempTxList.size()) {
+                right = tempTxList.get(index);
+            }
+           
+            String sha2HexValue = getSHA2HexValue(left + right);
+            newTxList.add(sha2HexValue);
+            index++;
+
+        }
+
+        return newTxList;
+    }
+
+    public String getSHA2HexValue(String str) {
+        byte[] cipher_byte;
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(str.getBytes());
+            cipher_byte = md.digest();
+            StringBuilder sb = new StringBuilder(2 * cipher_byte.length);
+            for(byte b: cipher_byte) {
+                sb.append(String.format("%02x", b&0xff) );
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public String getRoot() {
+        return this.root;
+    }
+}
